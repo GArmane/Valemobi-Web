f1da283e7bca35727dc0571feefe21f076a28f9e
Giovanni Armane <giovanniarmane@gmail.com>
2017-09-09 00:36:20 -0300

    Implementação cadastro de mercadorias

--- Controllers/MercadoriaController.cs
+++ Controllers/MercadoriaController.cs
@@ -1,32 +1,60 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ValemobiWeb.Data;
-using ValemobiWeb.Data.Access;
 using ValemobiWeb.Models;
 
 namespace ValemobiWeb.Controllers
 {
     public class MercadoriaController : Controller
     {
-        private readonly MercadoriaDAO _dao;
+        private readonly Context _context;
 
         public MercadoriaController(Context context)
         {
-            this._dao = new MercadoriaDAO(context);
+            this._context = context;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            var mercadorias = this._dao.GetList();
+            var mercadorias = this._context.Mercadorias.AsNoTracking().ToList();
             return View(new MercadoriaViewModel(mercadorias));
         }
 
         [HttpGet]
         public IActionResult Create()
         {
-            return NotFound();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(
+            [Bind("ID,Tipo,Nome,Quantidade,Preco,Negocio")] 
+            Mercadoria mercadoria
+        )
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    this._context.Add(mercadoria);
+                    await this._context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                ModelState.AddModelError(
+                    "Mercadoria", 
+                    "Não foi possível " + 
+                    "cadastrar uma nova mercadoria. " + 
+                    "Tente novamente mais tarde ou se o erro persistir, " + 
+                    "contate o administrador."
+                );
+            }
+            return View(mercadoria);
         }
 
         [HttpGet]
--- Models/Mercadoria.cs
+++ Models/Mercadoria.cs
@@ -1,3 +1,7 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace ValemobiWeb.Models
 {
     public enum TipoNegocio
@@ -9,10 +13,30 @@ namespace ValemobiWeb.Models
     public class Mercadoria
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage="Por favor, informe o tipo de mercadoria.")]
+        [StringLength(50, MinimumLength = 3, 
+        ErrorMessage="Tipo de mercadoria deve ter entre 3 a 100 caracteres.")]
         public string Tipo { get; set; }
+
+        [Required(ErrorMessage="Por favor, informe o nome da mercadoria.")]
+        [StringLength(100, MinimumLength = 3, 
+        ErrorMessage="Nome da mercadoria deve ter entre 3 a 100 caracteres.")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage="Por favor, informe a quantidade de itens.")]
+        [Range(1, Int32.MaxValue,
+        ErrorMessage = "Quantidade deve ser maior ou igual a 1.")]
         public int Quantidade { get; set; }
+
+        [Required(ErrorMessage="Por favor, informe o preço da mercadoria.")]
+        [Range(0, Double.MaxValue, 
+        ErrorMessage="Preço deve ser maior ou igual a 0")]
+        [DataType(DataType.Currency)]
         public decimal Preco { get; set; }
+
+        [Required(ErrorMessage="Por favor, informe a categoria da mercadoria.")]
+        [DisplayName("Categoria")]
         public TipoNegocio Negocio { get; set; }
 
         public Mercadoria() { }
